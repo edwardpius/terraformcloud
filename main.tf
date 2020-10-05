54ac31d09ebf6df48e705ae2233e14055f57b4e9
edwardpius <e.pius@computer.org>
2020-10-05 08:31:10 -0700

    Commented out stuff

--- main.tf
+++ main.tf
@@ -4,87 +4,87 @@ provider "consul" {
   token      = var.token
 }
 
-resource "consul_intention" "api-allow" {
-  source_name      = "frontend"
-  destination_name = "api"
-  action           = "allow"
-}
+# resource "consul_intention" "api-allow" {
+#   source_name      = "frontend"
+#   destination_name = "api"
+#   action           = "allow"
+# }
 
-resource "consul_intention" "db-allow" {
-  source_name      = "api"
-  destination_name = "db"
-  action           = "allow"
-}
+# resource "consul_intention" "db-allow" {
+#   source_name      = "api"
+#   destination_name = "db"
+#   action           = "allow"
+# }
 
-resource "consul_intention" "fe-v2-allow" {
-  source_name      = "frontend-v2"
-  destination_name = "api"
-  action           = "allow"
-}
+# resource "consul_intention" "fe-v2-allow" {
+#   source_name      = "frontend-v2"
+#   destination_name = "api"
+#   action           = "allow"
+# }
 
-resource "consul_intention" "ig-fe-allow" {
-  source_name      = "hcs-igw"
-  destination_name = "frontend"
-  action           = "allow"
-}
+# resource "consul_intention" "ig-fe-allow" {
+#   source_name      = "hcs-igw"
+#   destination_name = "frontend"
+#   action           = "allow"
+# }
 
-resource "consul_intention" "ig-fe2-allow" {
-  source_name      = "hcs-igw"
-  destination_name = "frontend-v2"
-  action           = "allow"
-}
+# resource "consul_intention" "ig-fe2-allow" {
+#   source_name      = "hcs-igw"
+#   destination_name = "frontend-v2"
+#   action           = "allow"
+# }
 
-resource "consul_config_entry" "frontend" {
-  name = "frontend"
-  kind = "service-defaults"
+# resource "consul_config_entry" "frontend" {
+#   name = "frontend"
+#   kind = "service-defaults"
 
-  config_json = jsonencode({
-    Protocol = "http"
-  })
-}
+#   config_json = jsonencode({
+#     Protocol = "http"
+#   })
+# }
 
-resource "consul_config_entry" "frontend-v2" {
-  name = "frontend-v2"
-  kind = "service-defaults"
+# resource "consul_config_entry" "frontend-v2" {
+#   name = "frontend-v2"
+#   kind = "service-defaults"
 
-  config_json = jsonencode({
-    Protocol = "http"
-  })
-}
+#   config_json = jsonencode({
+#     Protocol = "http"
+#   })
+# }
 
-resource "consul_config_entry" "fe-splitter" {
-  kind = "service-splitter"
-  name = consul_config_entry.frontend.name
+# resource "consul_config_entry" "fe-splitter" {
+#   kind = "service-splitter"
+#   name = consul_config_entry.frontend.name
 
-  config_json = jsonencode({
-    Splits = [
-      {
-        Weight = 100
-      },
-      {
-        Weight  = 0
-        Service = "frontend-v2"
-      }
-    ]
-  })
-}
+#   config_json = jsonencode({
+#     Splits = [
+#       {
+#         Weight = 100
+#       },
+#       {
+#         Weight  = 0
+#         Service = "frontend-v2"
+#       }
+#     ]
+#   })
+# }
 
-resource "consul_config_entry" "ingress" {
-  kind = "ingress-gateway"
-  name = "hcs-igw"
+# resource "consul_config_entry" "ingress" {
+#   kind = "ingress-gateway"
+#   name = "hcs-igw"
 
-  config_json = jsonencode({
-    Listeners = [
-      {
-        Port     = 8080
-        Protocol = "http"
-        Services = [
-          {
-            Name  = "frontend"
-            Hosts = ["*"]
-          }
-        ]
-      }
-    ]
-  })
-}
+#   config_json = jsonencode({
+#     Listeners = [
+#       {
+#         Port     = 8080
+#         Protocol = "http"
+#         Services = [
+#           {
+#             Name  = "frontend"
+#             Hosts = ["*"]
+#           }
+#         ]
+#       }
+#     ]
+#   })
+# }
